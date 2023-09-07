c4d9473f6c374771fb8c9bc6af22d967045537dd
AbeerAlemam <abeer.ict7@gmail.com>
2023-09-07 13:37:31 -0700

    #1 action

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the size of various types using size of funcation.
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	printf("size of a char: %u byte(s)\n", sizeof(char));
+	printf("size of an int: %u byte(s)\n", sizeof(int));
+	printf("size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("size of a float: %u byte(s)\n", sizeof(float));
+	return (0);
+}
