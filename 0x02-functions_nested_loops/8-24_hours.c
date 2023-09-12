85be1bb9aed81d6b58c56ec815a06d5ae45a8806
AbeerAlemam <abeer.ict7@gmail.com>
2023-09-12 15:55:18 -0700

    k

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer prints every minute of the day.
+ * jack_bauer prints every minute of the day
 */
 
 void jack_bauer(void)
@@ -16,7 +16,7 @@ void jack_bauer(void)
 			_putchar((hr % 10) + 48);
 			_putchar(':');
 			_putchar((min / 10) + 48);
-			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}
