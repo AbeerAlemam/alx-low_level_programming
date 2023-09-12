7a123836ec9f978d605807139f93ffae33d39988
AbeerAlemam <abeer.ict7@gmail.com>
2023-09-12 15:39:09 -0700

    k

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,6 +23,10 @@ void times_table(void)
 
 			prod = num * mult;
 
+			/*
+			 * put space if product is a single number
+			 * place the first digit if its tow numbers
+			*/
 			if (prod <= 9)
 				_putchar(' ');
 			else
