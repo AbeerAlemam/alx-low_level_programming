926f25fdb3b218c774cfa638e6a0ee921f9c9261
AbeerAlemam <abeer.ict7@gmail.com>
2023-09-15 16:28:13 -0700

    go abeer

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if c is uppercase
+ * @c:is input for character
+ *
+ * Return: 1 if it is upper, 0 is not
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+g
