1ae108a063fc7549ba62e1c7ce217ebf65219e8f
AbeerAlemam <abeer.ict7@gmail.com>
2023-09-15 18:36:37 -0700

    go abeer

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,24 +2,27 @@
 
 /**
  * print_diagonal - print a diagonal line
- *
- * @n: is the number of times the \ character
- *	should be printed
+ * @n: is the number of times the \ character should be printed
 */
 
 void print_diagonal(int n);
 {
-	int p, s;
-
 	if (n <= 0)
-		_putchar('\n');
-	else
 	{
-		for (p = 1; p <= n; p++)
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (s = 1; s <= p; s++)
-				_putchar(' ');
-			_putchar(92);
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
