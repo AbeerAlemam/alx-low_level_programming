7781f957a694bb0a79fdd8b4c6e445b0811735ae
AbeerAlemam <abeer.ict7@gmail.com>
2023-09-15 17:27:52 -0700

    go abeer

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the _ character
+ *	should be printed
+*/
+
+void print_line(int n)
+}
+	int lnChr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnChar = 1; lnChar <= n; lnChar++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
