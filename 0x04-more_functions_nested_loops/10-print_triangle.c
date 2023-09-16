3a354e606975133f9692ee82ee3bf3be47295b4c
AbeerAlemam <abeer.ict7@gmail.com>
2023-09-15 17:47:13 -0700

    go abeer

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using character #
+ *
+ * @size: is the size of the traingle
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_triangle(int size)
+{
+	int hight, base;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hight = 1; hight <= size; hight++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if ((hight + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
