6150048d43af153084741ca1529b14b7a4aa0707
AbeerAlemam <abeer.ict7@gmail.com>
2023-09-21 10:53:37 -0700

    0&1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,8 +16,9 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[c])
 		c++;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c = i] = src[i];
+		dest[c + i] = src[i];
 	dest[c + i] = '\0';
 
 	return (dest);
