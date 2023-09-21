f0d71b3a1ff44a9c265c592545ff33525bbe7c2a
AbeerAlemam <abeer.ict7@gmail.com>
2023-09-21 10:17:11 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * char *_strcat - concatenates two strings
+ *
+ * @src: point to source input
+ * @dest: point to destination input
+ * Return: a pointer to the resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c++] = src[c2]
+
+	return (dest);
+}
