5595f0e145033222a2c9eb4f17aca0e9c086d576
AbeerAlemam <abeer.ict7@gmail.com>
2023-10-02 12:05:01 -0700

    g

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * Return: 0
 */
 
-int main(int argc, char const *argv[])
+int main(int argc __attribute__((unsed)), char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv[0]);
+
 	return (0);
 }
