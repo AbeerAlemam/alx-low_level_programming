0951c6c152c4681b23c7000668dfd5f30b243803
AbeerAlemam <abeer.ict7@gmail.com>
2023-10-06 06:12:35 -0700

    o

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -6,9 +6,9 @@
  * @c: the character to print
  *
  * Return: On success 1.
- *	On error. -1 is returned. and errono is set appropriately
+ *      On error. -1 is returned. and errono is set appropriately
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+        return (write(1, &c, 1));
 }
