769e889547018303bb34e1eeae56f0da0c170501
AbeerAlemam <abeer.ict7@gmail.com>
2023-10-13 15:38:33 -0700

    x

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,22 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
 /**
  * sum_them_all - Returns the sum of all its paramters.
  * @n: The number of paramters passed to the function.
  * @...: A variable number of paramters to calculate the sum of.
  *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ * Return: intger
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i, sum = 0;
+	va_list a;
+	unsigned int i;
+	int sum = 0;
 
-	va_start(ap, n);
+	va_start(a, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-
-	va_end(ap);
-
+		sum += va_arg(a, int);
+	va_end(a);
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,7 @@
 #ifndef _VARIADIC_H_
 #define _VARIADIC_H_
 
-#include <stdio.h>
-#include <sdtarg.h>
-
-/*int _putchar(char c);*/
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
