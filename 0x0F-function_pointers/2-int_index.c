0e4320632b55ea107cd5bebb9d03ba106fb73978
AbeerAlemam <abeer.ict7@gmail.com>
2023-10-13 13:14:02 -0700

    x

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,12 +9,12 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	/** int i; */
+	int i;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-	for (int i = 0; i < size; i++)
+	for ( i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
