d394ee0b582e60af8d28a6e192c81a43dba5585e
AbeerAlemam <abeer.ict7@gmail.com>
2023-10-14 09:04:39 -0700

    sdf

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a function-like macro ABS(x) that computes
+ * the absolute value of a number x
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
